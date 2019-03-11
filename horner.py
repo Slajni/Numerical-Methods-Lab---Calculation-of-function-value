ec05f31e25f53b114da7edddcd66f2122b608943
Damian <c.damiankucharski@gmail.com>
2019-03-11 22:36:49 +0100

    added algorithm for normal evaluation

--- horner.py
+++ horner.py
@@ -2,7 +2,7 @@ def getCoefficients():
     """Returns a dictionary with coefficients"""
     coeffDict = {}
     num = 0
-    coeffGot = input("Give me the value of the coefficient next to x{} or write quit to leave: ".format(num))
+    coeffGot = input("Give me the value of the coefficient next to x^{} or write quit to leave: ".format(num))
     while coeffGot !='quit':
         try:
             val = int(coeffGot)
@@ -11,8 +11,24 @@ def getCoefficients():
         except ValueError:
             print("This is not a valid input")
         
-        coeffGot = input("Give me the value of the coefficient next to x{} or write quit to leave: ".format(num))
+        coeffGot = input("Give me the value of the coefficient next to x^{} or write quit to leave: ".format(num))
 
 
     return coeffDict
     
+
+
+
+valueOfX = 10   # hardcoded value of x in polynomial
+
+
+def normalEvaluation(coefs,val):
+    """Evaluates function value in standard way"""
+    valToReturn = 0
+    for key, value in coefs.items():
+        valToReturn += val**key * value
+    return valToReturn
+
+print(normalEvaluation(getCoefficients(),valueOfX))
+
+
